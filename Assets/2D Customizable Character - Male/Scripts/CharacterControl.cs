0b2dfce3fd05816fd4d20cd4fef73c17a48937ff
Scottie Neumeyer <sneumeyer@mtdproducts.com>
2018-03-25 22:37:50 -0400

    Initial Commit

---
+++ Assets/2D Customizable Character - Male/Scripts/CharacterControl.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+
+    public GameObject downDir, upDir, leftDir, rightDir;
+    public GameObject currentDir;
+    public float speed = 7;
+    public bool isWalking;
+    public float horizontal, vertical;
+ 
+    // Use this for initialization
+    void Start()
+    {
+        if (currentDir == null)
+            currentDir = downDir;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveDirection = Vector2.zero;
+        //currentDirAnim = currentDir.GetComponent<Animator>();
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        isWalking = false;
+
+        if (horizontal < 0)
+        {
+            isWalking = true;
+            moveDirection.x = horizontal;
+
+            if (!currentDir.Equals(leftDir) || currentDir.GetComponent<Animator>().GetBool("Idle"))
+            {
+                currentDir.SetActive(false);
+                currentDir = leftDir;
+                currentDir.SetActive(true);
+                currentDir.GetComponent<Animator>().Play(0);
+                currentDir.GetComponent<Animator>().SetBool("Idle", false);
+            }  
+        }
+        else if (vertical > 0)
+        {
+            isWalking = true;
+            moveDirection.y = 1;
+
+            if (!currentDir.Equals(upDir))
+            {
+                currentDir.SetActive(false);
+                currentDir = upDir;
+                currentDir.SetActive(true);
+                currentDir.GetComponent<Animator>().Play(0);
+                currentDir.GetComponent<Animator>().SetBool("Idle", false);
+            }
+        }
+        else if (horizontal > 0)
+        {
+            isWalking = true;
+            moveDirection.x = 1;
+
+            if (!currentDir.Equals(rightDir))
+            {
+                currentDir.SetActive(false);
+                currentDir = rightDir;
+                currentDir.SetActive(true);
+                currentDir.GetComponent<Animator>().Play(0);
+                currentDir.GetComponent<Animator>().SetBool("Idle", false);
+            }
+        }
+        else if (vertical < 0)
+        {
+            isWalking = true;
+            moveDirection.y = -1;
+
+            if (currentDir != downDir)
+            {
+                currentDir.SetActive(false);
+                currentDir = downDir;
+                currentDir.SetActive(true);
+                currentDir.GetComponent<Animator>().Play(0);
+                currentDir.GetComponent<Animator>().SetBool("Idle", false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            currentDir.GetComponent<Animator>().SetTrigger("Thrust");
+            moveDirection.x = 0;
+            moveDirection.y = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveDirection.x = 0;
+            moveDirection.y = 0;
+            currentDir.GetComponent<Animator>().SetTrigger("Swing");
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            moveDirection.x = 0;
+            moveDirection.y = 0;
+            currentDir.GetComponent<Animator>().SetTrigger("Bow");
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            moveDirection.x = 0;
+            moveDirection.y = 0;
+            currentDir.GetComponent<Animator>().SetTrigger("Spell");
+        }
+
+        // detect for idle state, or move character
+        if (!isWalking)
+        {
+            currentDir.GetComponent<Animator>().SetBool("Idle", true);
+        }
+        else
+            transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+
+
+}
---
+++ Assets/2D Customizable Character - Male/Scripts/CharacterMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public float speed = 5;
+    private Animator animator;
+    private GameObject character;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        character = GameObject.Find("Knight Character");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveDirection = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveDirection.x = -1;
+            character.transform.localScale.Set(-1, 1, 1);
+            animator.SetInteger("Direction", 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveDirection.y = 1;
+            animator.SetInteger("Direction", 1);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveDirection.x = 1;
+            animator.SetInteger("Direction", 2);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveDirection.y = -1;
+            animator.SetInteger("Direction", 3);
+        }
+
+        // look for attack commands
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetTrigger("Swing");
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+            animator.SetTrigger("Thrust");
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            animator.SetTrigger("Bow");
+        }
+
+        // detect idle state
+        if ((Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.y)) == 0)
+        {
+            animator.SetBool("Idle", true);
+        }
+        else
+        {
+            animator.SetBool("Idle", false);
+            character.transform.Translate(moveDirection * speed * Time.deltaTime);
+        }
+
+    }
+}
